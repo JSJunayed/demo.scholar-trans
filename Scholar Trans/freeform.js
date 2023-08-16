1729c5e7e5dbc6ad8f025f81f1a9d965a9e18a85
JSJunayed <aliibnsayeed.junayed@gmail.com>
2023-08-16 17:33:59 +0600

    demo.scholar-trans

---
+++ Scholar Trans/freeform.js
@@ -0,0 +1,15 @@
+//const myForm = document.querySelector('#freeform');
+//const nameInput = document.querySelector('#first-name');
+//const textInput = document.querySelector('#last-name');
+//const emailInput = document.querySelector('#email1');
+//const userList = document.querySelector('#users');
+
+
+
+//getElementbyId("#submit").submit(function(){
+    //getElementbyId("#freeform").Onsubmit(function(){
+        //alert("Submitted");
+    //});
+//});
+    
+
